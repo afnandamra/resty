bd3500a11bf7fb5cc6f8f26ac2e1c091f0a39c48
afnandamra <damra.afnan@gmail.com>
2021-05-05 23:39:24 +0300

    lab 29

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,7 +5,6 @@ import './home.scss';
 import Form from '../form/form';
 import History from '../history/history';
 import Results from '../results/results';
-import historyClick from '../../helper/historyClick';
 
 const superagent = require('superagent');
 
@@ -126,3 +125,21 @@ function uniqueArr(arr) {
   }
   return newArr;
 }
+
+async function historyClick(e) {
+  let method = e.currentTarget.childNodes[0].innerHTML;
+  let url = e.currentTarget.childNodes[1].innerHTML;
+  let body = e.currentTarget.childNodes[2].innerHTML;
+
+  const input = document.getElementById(`url`);
+  input.value = url;
+
+  const selected = document.getElementById(`${method}`);
+  await selected.click();
+
+  const text = document.getElementById('body');
+  text.value = body;
+
+  const submit = document.getElementById('submit');
+  await submit.click();
+};
